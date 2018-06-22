1d2b21a4445f91a278a6e8103ac34b99084896d2
mingyu <rlaalsrbgk@gmail.com>
2018-06-22 16:53:17 +0900

    logout view 구현

--- app/members/urls.py
+++ app/members/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import login_view
+from .views import login_view, logout_view
 
 app_name = 'members'
 urlpatterns =[
-    path('login/', login_view, name='login')
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
--- app/members/views.py
+++ app/members/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import authenticate
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+
+from posts.views import index
 
 
 def login_view(request):
@@ -21,3 +23,10 @@ def login_view(request):
     else:
         return render(request, 'members/login.html')
 
+
+def logout_view(request):
+
+    if request.method == 'POST':
+
+        logout(request)
+        return redirect(index)
