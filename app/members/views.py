7a53a4d5ed8b6654e7df749bf2af79b91a4d5387
mingyu <rlaalsrbgk@gmail.com>
2018-06-23 16:05:03 +0900

    signup view 구현 User에 있을경우 오류 반환 아닐경우에는 user을 만들고 로그인

--- app/members/views.py
+++ app/members/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.contrib.auth.views import login, logout
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
@@ -30,3 +31,23 @@ def logout_view(request):
 
         logout(request)
         return redirect(index)
+
+
+def signup_view(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            return redirect('members:signup')
+        else:
+            create_user = User.objects.create_user(username=username, password=password)
+            create_user.save()
+            login(request, user)
+            return redirect('posts:post-list')
+    else:
+        return render(request, 'members/signup.html')
+
