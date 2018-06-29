76fa5f0dd784439247a46f97f24322f1b4bf0512
mingyu <rlaalsrbgk@gmail.com>
2018-06-29 15:07:11 +0900

    테스트 코드 작성 및 models.py 수정

--- app/members/models.py
+++ app/members/models.py
@@ -12,7 +12,85 @@ class User(AbstractUser):
     site = models.URLField(blank=True)
     introduce = models.TextField(blank=True)
     gender = models.CharField(max_length=1, choices=CHOICES_GENDER)
+    to_relation_user = models.ManyToManyField(
+        'self',
+        through='Relation',
+        symmetrical=False,
+        blank=True,
+        related_name='from_relation_users',
+        related_query_name='form_relation_user',
+    )
 
     def __str__(self):
         return self.username
 
+    def follow(self, another_user):
+        return self.relations_by_from_user.create(
+            to_user=another_user,
+            relation_type=Relation.RELATION_TYPE_FOLLOW,
+        )
+
+    @property
+    def following(self):
+        return User.objects.filter(
+            relations_by_to_user__from_user=self,
+            relations_by_to_user__relation_type=Relation.RELATION_TYPE_FOLLOW,
+        )
+
+
+    @property
+    def followers(self):
+        return User.objects.filter(
+            relations_by_to_from__to_user=self,
+            relations_by_to_from__relation_type=Relation.RELATION_TYPE_FOLLOW,
+        )
+
+    @property
+    def block_user(self):
+        return User.objects.filter(
+            relations_by_to_user__from_user=self,
+            relations_by_to_user__relation_type=Relation.RELATION_TYPE_BLOCK,
+        )
+
+    @property
+    def following_relation(self):
+        return self.relations_by_from_user.filter(relation_type=Relation.RELATION_TYPE_FOLLOW)
+
+    @property
+    def follower_relation(self):
+        return self.relations_by_to_user.filter(relation_type=Relation.RELATION_TYPE_FOLLOW)
+
+    @property
+    def block_relation(self):
+        return self.relations_by_from_user.filter(relation_type=Relation.RELATION_TYPE_BLOCK)
+
+
+class Relation(models.Model):
+    RELATION_TYPE_BLOCK = 'b'
+    RELATION_TYPE_FOLLOW = 'f'
+
+    CHOICE_RELATION_TYPE = (
+        (RELATION_TYPE_FOLLOW, 'Follow'),
+        (RELATION_TYPE_BLOCK, 'Block'),
+    )
+
+    from_user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='relations_by_from_user'
+    )
+
+    to_user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='relations_by_to_user'
+    )
+
+    relation_type = models.CharField(max_length=1, choices=CHOICE_RELATION_TYPE)
+
+    def __str__(self):
+        return 'From {from_user} to {to_user} ({type})'.format(
+            from_user=self.from_user.username,
+            to_user=self.to_user.username,
+            type=self.get_relation_type_display(),
+        )
--- app/members/tests.py
+++ app/members/tests.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+User = get_user_model()
+
+
+class RelationTestCase(TestCase):
+
+    def test_follow(self):
+        u1 = User.objects.create_user(username='u1')
+        u2 = User.objects.create_user(username='u2')
+
+        u1.follow(u2)
+        # u1.relations_by_from_user.create(to_user=u2, relation_type='f')
+
+        self.assertIn(u2, u1.following)
+
